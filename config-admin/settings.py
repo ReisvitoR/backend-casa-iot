a2036ec96188d6255c5acfbc7f96ec2f47ad884e
ReisvitoR <reishuvitor@gmail.com>
2025-08-29 17:16:02 -0300

    descobrindo-erro-test

--- config-admin/settings.py
+++ config-admin/settings.py
@@ -111,7 +111,7 @@ SECRET_KEY = 'django-insecure-ws47k7v^pv5c_=2p@42-8=ok+qk^+68kw4e#lmofiocju434-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'backend-casa-iot.onrender.com', '*.onrender.com']
+ALLOWED_HOSTS = ['*']  # Temporário para debug - aceita todos os hosts
 
 
 # Application definition
